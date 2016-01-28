1c948553537fe77ee800e886910dc2ed1f96d174
Ulric Villeneuve <ulric.villeneuve@polymtl.ca>
2016-01-27 22:59:49 -0500

    Selection sur une ligne de differente grandeur fonctionnelle, mais pas sur une ligne en angle

--- Cadriciel/Sources/DLL/Visiteur/VisiteurSelection.cpp
+++ Cadriciel/Sources/DLL/Visiteur/VisiteurSelection.cpp
@@ -39,39 +39,81 @@ void VisiteurSelection::visiter(NoeudTable* noeud)
 	{
 		modele::Modele3D const* modeleEnfant = noeud->chercher(i)->getModele();
 		utilitaire::BoiteEnglobante boite = utilitaire::calculerBoiteEnglobante(*modeleEnfant);
-		/*
-		float boiteMinX = boite.coinMin[0], boiteMinY = boite.coinMin[1], boiteMinZ = boite.coinMin[2], boiteMaxX = boite.coinMax[0], boiteMaxY = boite.coinMax[1], boiteMaxZ = boite.coinMax[2];
 
-		glm::dvec3 origine, pointLigne;
+		boite.coinMin[1] -= 0.5;
+		boite.coinMax[1] += 0.5;
+
+		boite.coinMax[1] *= noeud->chercher(i)->obtenirFacteurDimension()/2 * 1.10;
+		boite.coinMin[1] *= noeud->chercher(i)->obtenirFacteurDimension()/2 *1.10;
+
+		/*std::cout << "Boite apres scale : " << boite.coinMin[1] << " , " << boite.coinMax[1] << std::endl;
+		std::cout << "Curseur : " << positionRelative_[1] << std::endl;
+		std::cout << "Limite inf: " << boite.coinMin[1] + noeud->chercher(i)->obtenirPositionRelative()[1] << std::endl;
+		std::cout << "Limite sup: " << boite.coinMax[1] + noeud->chercher(i)->obtenirPositionRelative()[1] << std::endl;*/
+
+		
+		float boiteMinX = boite.coinMin[0], boiteMinY = boite.coinMin[1], boiteMaxX = boite.coinMax[0], boiteMaxY = boite.coinMax[1];
+		//	boiteMinZ = boite.coinMin[2],  
+		//	boiteMaxZ = boite.coinMax[2];
+
+		glm::dvec3 origine, pointBoite1, pointBoite2, pointBoite3, pointBoite4;
 		origine[0] = 0.0;
 		origine[1] = 0.0;
 		origine[2] = 0.0;
 
-		pointLigne[0] = noeud->chercher(i)->obtenirPositionRelative()[0];
-		pointLigne[1] = noeud->chercher(i)->obtenirPositionRelative()[1];
-		pointLigne[2] = noeud->chercher(i)->obtenirPositionRelative()[2];
+		pointBoite1[0] = boite.coinMin[0];
+		pointBoite1[1] = boite.coinMin[1];
 
-		double r;
-		r = utilitaire::calculerDistanceHypothenuse(origine, pointLigne);
+		pointBoite2[0] = boite.coinMax[0];
+		pointBoite2[1] = boite.coinMin[1];
 
-		double angleRotation = noeud->chercher(i)->obtenirAngleRotation();
+		pointBoite3[0] = boite.coinMax[0];
+		pointBoite3[1] = boite.coinMax[1];
 
-		boiteMinX = r*cos(angleRotation);
-		boiteMinY = r*sin(angleRotation);
-
-		boiteMaxX = 
-
-		boite.coinMin[0] *= noeud->chercher(i)->obtenirFacteurDimension();
-		boite.coinMin[1] *= noeud->chercher(i)->obtenirFacteurDimension();
-		boite.coinMin[2] *= noeud->chercher(i)->obtenirFacteurDimension();
-
-		boite.coinMax[0] *= noeud->chercher(i)->obtenirFacteurDimension();
-		boite.coinMax[1] *= noeud->chercher(i)->obtenirFacteurDimension();
-		boite.coinMax[2] *= noeud->chercher(i)->obtenirFacteurDimension();
-		*/
+		pointBoite4[0] = boite.coinMin[0];
+		pointBoite4[1] = boite.coinMax[1];
 		
 
-		if (positionRelative_[0] < boite.coinMin[0] + noeud->chercher(i)->obtenirPositionRelative()[0] || positionRelative_[0] > boite.coinMax[0] + noeud->chercher(i)->obtenirPositionRelative()[0] || positionRelative_[1] < boite.coinMin[1] + noeud->chercher(i)->obtenirPositionRelative()[1] || positionRelative_[1] > boite.coinMax[1] + noeud->chercher(i)->obtenirPositionRelative()[1])
+		double r1, r2, r3, r4;
+		r1 = utilitaire::calculerDistanceHypothenuse(origine, pointBoite1);
+		r2 = utilitaire::calculerDistanceHypothenuse(origine, pointBoite2);
+		r3 = utilitaire::calculerDistanceHypothenuse(origine, pointBoite3);
+		r4 = utilitaire::calculerDistanceHypothenuse(origine, pointBoite4);
+
+		double angleRotation = noeud->chercher(i)->obtenirAngleRotation();
+		pointBoite1[0] = r1*cos(angleRotation-180);
+		pointBoite1[1] = r1*sin(angleRotation-180);
+
+		pointBoite2[0] = r2*cos(angleRotation-180);
+		pointBoite2[1] = r2*sin(angleRotation-180);
+
+		pointBoite3[0] = r3*cos(angleRotation);
+		pointBoite3[1] = r3*sin(angleRotation);
+
+		pointBoite4[0] = r4*cos(angleRotation);
+		pointBoite4[1] = r4*sin(angleRotation);
+
+		pointBoite1[0] = noeud->chercher(i)->obtenirPositionRelative()[0] + pointBoite1[0];
+		pointBoite1[1] = noeud->chercher(i)->obtenirPositionRelative()[1] + pointBoite1[1];
+		pointBoite1[2] = 0.0;
+
+		pointBoite2[0] = noeud->chercher(i)->obtenirPositionRelative()[0] + pointBoite2[0];
+		pointBoite2[1] = noeud->chercher(i)->obtenirPositionRelative()[1] + pointBoite2[1];
+		pointBoite2[2] = 0.0;
+
+		pointBoite3[0] = noeud->chercher(i)->obtenirPositionRelative()[0] + pointBoite3[0];
+		pointBoite3[1] = noeud->chercher(i)->obtenirPositionRelative()[1] + pointBoite3[1];
+		pointBoite3[2] = 0.0;
+
+		pointBoite4[0] = noeud->chercher(i)->obtenirPositionRelative()[0] + pointBoite4[0];
+		pointBoite4[1] = noeud->chercher(i)->obtenirPositionRelative()[1] + pointBoite4[1];
+		pointBoite4[2] = 0.0;
+
+		//boiteMinX = r*cos(angleRotation);
+		//boiteMinY = r*sin(angleRotation);
+
+		//if (positionRelative_[0] < boite.coinMin[0] + noeud->chercher(i)->obtenirPositionRelative()[0] || positionRelative_[0] > boite.coinMax[0] + noeud->chercher(i)->obtenirPositionRelative()[0] || positionRelative_[1] < boite.coinMin[1] + noeud->chercher(i)->obtenirPositionRelative()[1] || positionRelative_[1] > boite.coinMax[1] + noeud->chercher(i)->obtenirPositionRelative()[1])
+		if (positionRelative_[0] < pointBoite2[0] || positionRelative_[0] > pointBoite1[0] || positionRelative_[1] < pointBoite3[1] || positionRelative_[1] > pointBoite1[1])
 		{
 			noeud->chercher(i)->assignerSelection(0);
 		}
