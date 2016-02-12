bcce8219a8a67b622611807b5c3f08108480df93
cagena <camille.gendreau@polymtl.ca>
2016-02-11 22:45:15 -0500

    Creation du modele et des noeuds, non-fonctionel

---
+++ Cadriciel/Sources/DLL/NoeudJonction.cpp
@@ -0,0 +1,114 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file NoeudPoteau.cpp
+/// @author Camille Gendreau
+/// @date 2016-02-11
+/// @version 1.0
+///
+/// @addtogroup inf2990 INF2990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
+#include "NoeudJonction.h"
+#include "Utilitaire.h"
+
+#include "GL/glew.h"
+#include <cmath>
+#include <iostream>
+
+#include "Modele3D.h"
+#include "OpenGL_VBO.h"
+
+#include "VisiteurAbstrait.h"
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn NoeudPoteau::NoeudPoteau(const std::string& typeNoeud)
+///
+/// Ce constructeur ne fait qu'appeler la version de la classe et base
+/// et donner des valeurs par défaut aux variables membres.
+///
+/// @param[in] typeNoeud : Le type du noeud.
+///
+/// @return Aucune (constructeur).
+///
+////////////////////////////////////////////////////////////////////////
+NoeudJonction::NoeudJonction(const std::string& typeNoeud)
+	: NoeudAbstrait{ typeNoeud }
+{
+}
+
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn NoeudPoteau::~NoeudPoteau()
+///
+/// Ce destructeur désallouee la liste d'affichage du cube.
+///
+/// @return Aucune (destructeur).
+///
+////////////////////////////////////////////////////////////////////////
+NoeudJonction::~NoeudJonction()
+{
+}
+
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn void NoeudPoteau::afficherConcret() const
+///
+/// Cette fonction effectue le véritable rendu de l'objet.
+///
+/// @return Aucune.
+///
+////////////////////////////////////////////////////////////////////////
+void NoeudJonction::afficherConcret() const
+{
+	// Appel à la version de la classe de base pour l'affichage des enfants.
+	//	NoeudComposite::afficherConcret();
+
+	// Sauvegarde de la matrice.
+	glPushMatrix();
+
+	if (estSelectionne())
+	{
+		glColor4f(1.0, 0.0, 0.0, 1.0);
+	}
+	else
+	{
+		glColor4f(0.0, 1.0, 0.0, 1.0);
+	}
+
+	//glScaled(facteurMiseAEchelle_, facteurMiseAEchelle_, 1);
+
+
+	//glRotatef(90, 1, 0, 0);
+	//glRotatef(90, 0, 1, 0);
+
+	// Affichage du modèle.
+	vbo_->dessiner();
+
+	// Restauration de la matrice.
+	glPopMatrix();
+}
+
+void NoeudJonction::mettreAJourQuadEnglobantConcret(const glm::dvec3& positionRelative)
+{
+	// Mettre à jour les coins avec le facteur de mise à échelle.
+	std::cout << "Mise à jour Poteau:" << std::endl;
+	for (int i = 0; i < 4; i++)
+	{
+		quadEnglobant_.coins[i] *= facteurMiseAEchelle_;
+		quadEnglobant_.coins[i] += positionRelative_ + positionRelative;
+		std::cout << "coin " << i << ": " << quadEnglobant_.coins[i].x << ", " << quadEnglobant_.coins[i].y << std::endl;
+	}
+}
+
+
+void NoeudJonction::accepterVisiteur(VisiteurAbstrait* visiteur)
+{
+	visiteur->visiter(this);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// @}
+///////////////////////////////////////////////////////////////////////////////
