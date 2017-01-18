7bc3c14ad4b643a3aed7d8ea25d2f3393e733212
Philippe Marcotte <philippemarcotte@Philippes-Mac.local>
2017-01-17 23:06:56 -0500

    debut du UI

---
+++ prototype_ios/chatApp/ChatCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChatCell.swift
+//  chatApp
+//
+//  Created by Philippe Marcotte on 2017-01-17.
+//  Copyright © 2017 Philippe Marcotte. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell
+{
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myCellLabbel: UILabel!
+}
