df1c063c92247864bc6206090e7b8a368948d594
Philippe Marcotte <philippemarcotte@Philippes-Mac.local>
2017-01-17 20:59:04 -0500

    ajout du projet prototype ios

---
+++ prototype_ios/chatApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by Philippe Marcotte on 2017-01-17.
+//  Copyright © 2017 Philippe Marcotte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
