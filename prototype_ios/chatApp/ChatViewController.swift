d0b0258821c480d873ca13c5187b56c4c7d153ed
Philippe Marcotte <philippe.marcotte@polymtl.ca>
2017-01-18 15:51:13 -0500

    TableView, textfield et send button ajoutes. Textfield toujours cache lorsque clavier apparait.

---
+++ prototype_ios/chatApp/ChatViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by Philippe Marcotte on 2017-01-17.
+//  Copyright © 2017 Philippe Marcotte. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate {
+
+    let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
+    
+    let textCellIdentifier = "ChatCell"
+    
+    @IBOutlet weak var chatSendButtonHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var chatBoxHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var chatBoxView: UITextView!
+    @IBOutlet weak var chatSendButt: UIButton!
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chatTableView.delegate = self
+        chatTableView.dataSource = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardNotification(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ chatTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count
+    }
+    
+    func tableView(_ chatTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatTableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath as IndexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = swiftBlogs[row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+        let row = indexPath.row
+        print(swiftBlogs[row])
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if (endFrame?.origin.y)! >= UIScreen.main.bounds.size.height {
+                self.chatSendButtonHeightConstraint?.constant = 0.0
+                self.chatBoxHeightConstraint?.constant = 0.0
+            } else {
+                self.chatSendButtonHeightConstraint?.constant = endFrame?.size.height ?? 0.0
+                self.chatBoxHeightConstraint?.constant = endFrame?.size.height ?? 0.0
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
--- prototype_ios/chatApp/ChatViewControllerTableViewController.swift
+++ prototype_ios/chatApp/ChatViewControllerTableViewController.swift
@@ -8,47 +8,7 @@
 
 import UIKit
 
-class ChatViewControllerTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:ChatCell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! ChatCell
-
-        // Configure the cell...
-        cell.myView.backgroundColor = UIColor.blue
-        cell.myCellLabbel.text = "Heyo"
-
-        return cell
-    }
-
-
+class ChatViewControllerTableViewController: UITableView {
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
