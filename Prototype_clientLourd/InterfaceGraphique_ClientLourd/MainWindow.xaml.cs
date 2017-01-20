08479792bb848ba034de9e418e18b49f26bd24d9
Frederic Gregoire <frederic.gregoire@polymtl.ca>
2017-01-20 18:25:21 -0500

    Ajout de fonctionnalité send et enter. Ajout de verification user et password. Ajout de timestamp et bon format daffichage chat

--- Prototype_clientLourd/InterfaceGraphique_ClientLourd/MainWindow.xaml.cs
+++ Prototype_clientLourd/InterfaceGraphique_ClientLourd/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,24 +14,46 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//TODO: Enlever les users quand leave chat
+//TODO: Faire leave chat un bouton séparé
+//TODO: Remplacer "user" dans le timestamp
+
 namespace InterfaceGraphique_ClientLourd
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        const string chatTextBoxLabel = "Enter your message here";
+
         public MainWindow()
         {
             InitializeComponent();
             switchScreen(this);
-            showChatScreen();
+            showLoginScreen();
         }
 
         private void connectButton_Click(object sender, RoutedEventArgs e)
         {
-            switchScreen(this);
-            showChatScreen();
+            if (username_textbox.Text.Equals(""))
+            {
+                username_label_warning.Visibility = Visibility.Visible;
+                username_label_warning.Content = "Please enter your username.";
+            }
+            else if (password_textbox.Password.Equals(""))
+            {
+                password_label_warning.Visibility = Visibility.Visible;
+                password_label_warning.Content = "Please enter your username.";
+            }
+            else
+            {
+                //TODO: Verify the user is unique
+                //TODO: Add connection to server here
+               
+                switchScreen(this);
+                showChatScreen(username_textbox.Text);
+
+                username_textbox.Text = "";
+                password_textbox.Password = "";
+            }
         }
 
         private void switchScreen(UIElement parent)
@@ -59,7 +82,7 @@ namespace InterfaceGraphique_ClientLourd
             login_image.Visibility = Visibility.Visible;
         }
 
-        private void showChatScreen()
+        private void showChatScreen(String username)
         {
             chat_menu.Visibility = Visibility.Visible;
 
@@ -75,12 +98,17 @@ namespace InterfaceGraphique_ClientLourd
 
             Grid.SetColumn(chat_textBox, 3);
             Grid.SetColumnSpan(chat_textBox, 1);
+
+            //Add user
+            users_listBox.Items.Add(username);
         }
 
         private void leaveChatMenuItem_Click(object sender, RoutedEventArgs e)
         {
             switchScreen(this);
             showLoginScreen();
+
+            //TODO: Disconnect from server here
         }
 
         private void displayUserListChatMenuItem_Click(object sender, RoutedEventArgs e)
@@ -112,7 +140,7 @@ namespace InterfaceGraphique_ClientLourd
 
         private void chat_textBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (chat_textBox.Text == "Enter your message here")
+            if (chat_textBox.Text == chatTextBoxLabel)
             {
                 chat_textBox.Text = "";
             }
@@ -124,7 +152,40 @@ namespace InterfaceGraphique_ClientLourd
             if (chat_textBox.Text.Length == 0)
             {
                 chat_textBox.Foreground = Brushes.Gray;
-                chat_textBox.Text = "Enter your message here";
+                chat_textBox.Text = chatTextBoxLabel;
+            }
+        }
+
+        private void send_button_Click(object sender, RoutedEventArgs e)
+        {
+            bool isToolTip = chat_textBox.Text == chatTextBoxLabel;
+            bool noContentInText = (chat_textBox.Text == "" || chat_textBox.Text.Replace(" ", "") == "");
+            if (!isToolTip && !noContentInText)
+            {
+                string time = "User sent message at " + DateTime.Now.ToString();
+                chat_listBox.Items.Add(time + "\r" + chat_textBox.Text);
+
+                //Make scroll bar move to the bottom
+                Border border = (Border)VisualTreeHelper.GetChild(chat_listBox, 0);
+                ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                scrollViewer.ScrollToBottom();
+
+                chat_textBox.Text = "";
+            }
+        }
+
+        private void chat_textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+                send_button_Click(sender, e);
+        }
+
+        private void chat_listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Selector selector = sender as Selector;
+            if (selector is ListBox)
+            {
+                (selector as ListBox).ScrollIntoView(selector.SelectedItem);
             }
         }
     }
