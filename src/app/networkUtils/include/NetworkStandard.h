2f4de117750445a4f6d7316317b5f1f6f7ca475f
CGendreau <gendreau.camille101@gmail.com>
2017-03-07 14:55:35 -0500

    Added msg size header reading and appending functions to NetworkStandard

--- src/app/networkUtils/include/NetworkStandard.h
+++ src/app/networkUtils/include/NetworkStandard.h
@@ -8,6 +8,21 @@ namespace Networking {
 		const int SYSTEM = 4;
 		const int COMMAND = 5;
 		const int DATA_START = 6;
+
+		void AddMessageLengthHeader(std::string& message) {
+			int length = message.size();
+			std::string lengthHeader(static_cast<char*>(static_cast<void*>(&length)), sizeof length);
+			// optimal string concatenation modified message
+			message = (lengthHeader += message);
+		}
+
+		int GetMessageLength(std::string& message) {
+			int length = int((unsigned char)(message[0]) << 24 |
+				(unsigned char)(message[1]) << 16 |
+				(unsigned char)(message[2]) << 8 |
+				(unsigned char)(message[3]));
+			return 0;
+		}
 	}
 }
 
