5705693f163377dc1209086bacaa8eee27c49a9f
CGendreau <gendreau.camille101@gmail.com>
2017-03-07 00:52:27 -0500

    Added network standards and updated doc

---
+++ src/app/networkUtils/include/NetworkStandard.h
@@ -0,0 +1,10 @@
+namespace Network {
+	namespace MessageStandard {
+		//Index of the system indicator byte in the message
+		const int LENGTH_END = 0;
+		const int LENGTH_BEGIN = 3;
+		const int SYSTEM = 4;
+		const int COMMAND = 5;
+		const int DATA_START = 6;
+	}
+}
